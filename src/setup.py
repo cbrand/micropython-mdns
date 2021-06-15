b1a9473cd5200e97ee578be4c623bbd610f46b6c
Christoph Brand <christoph@xcnt.io>
2021-06-15 18:41:51 +0200

    Version 1.0.1

--- src/setup.py
+++ src/setup.py
@@ -18,7 +18,7 @@ def list_packages(source_directory: str = ".") -> List[str]:
     return packages
 
 
-version = "1.0.0"
+version = "1.0.1"
 requirements = []
 test_requirements = ["twine", "adafruit-ampy>=1.0.0"]
 
